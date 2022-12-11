6c155f1ebab34fa2f5010dea5cfb644f6d404fc4
artyom vasich <vasich.dev@gmail.com>
2022-12-12 01:27:00 +0400

    day 01

---
+++ src/day_01_calorie_counting.rs
@@ -0,0 +1,51 @@
+use itertools::Itertools;
+
+fn max_calories(filename: &str, n: usize) -> u32 {
+    let mut totals = vec![];
+    let mut sum = 0;
+    let lines = crate::utils::read_lines(filename);
+
+    for line in lines {
+        if line.is_empty() {
+            totals.push(sum);
+            sum = 0;
+        } else {
+            sum += line.parse::<u32>().unwrap();
+        }
+    }
+
+    totals.push(sum);
+    totals.iter().sorted_by(|a, b| b.cmp(a)).take(n).sum()
+}
+
+#[cfg(test)]
+mod d01_test {
+    use super::*;
+
+    static EXAMPLE_1: &str = "./inputs/day_01/part_1_example.txt";
+    static TASK: &str = "./inputs/day_01/task.txt";
+
+    #[test]
+    fn part_one_example_test() {
+        let res = max_calories(EXAMPLE_1, 1);
+        assert_eq!(res, 24000)
+    }
+
+    #[test]
+    fn run_part_one_task() {
+        let res = max_calories(TASK, 1);
+        println!("{res}");
+    }
+
+    #[test]
+    fn part_two_example_test() {
+        let res = max_calories(EXAMPLE_1, 3);
+        assert_eq!(res, 45000)
+    }
+
+    #[test]
+    fn run_part_two_task() {
+        let res = max_calories(TASK, 3);
+        println!("{res}");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(test)]
 
+mod day_01_calorie_counting;
 mod day_09_rope_bridge;
 mod day_10_cathode_ray_tube;
 mod day_11_monkey_in_the_middle;
